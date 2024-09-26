ffcd3f210b65d84aa75d8ae0f7be8f27415bf021
Angelina Ruiz <20561509-S@estudiants.urv.cat>
2024-09-26 16:51:28 +0200

    Inicializacion ficheros

--- main.py
+++ main.py
@@ -2,13 +2,27 @@ import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt #per poder mostrar els grafics
 
+# Configurar pandas para mostrar todas las columnas
+#pd.set_option('display.max_columns', None)
+
 #Carregar les dades
 dataBcn = pd.read_csv("CityFiles/barcelona/listings.csv")
 dataBer = pd.read_csv("CityFiles/berlin/listings.csv")
 dataBuAr = pd.read_csv("CityFiles/buenos aires/listings.csv")
 
-print(dataBcn.columns)
-print(dataBcn.dtypes)
+print(dataBcn.head())
+print(dataBcn.info())
 
 dataBcn.hist('availability_365')
 plt.show()
+
+
+
+#Histograma relaciones
+dataBcn.plot(kind='scatter', x='accommodates', y='availability_365', alpha=0.5)
+plt.title("Relación entre el Precio y la Disponibilidad")
+plt.xlabel("Accommodates")
+plt.ylabel("Días disponibles al año")
+plt.show()
+
+dataBcn.to_csv("CityFiles/barcelona/tarnsformado.csv", sep = ';', index=True)
