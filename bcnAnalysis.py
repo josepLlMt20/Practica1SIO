9699859c8d8954e0de89991c1fe8fbc49f45d535
Angelina Ruiz <125549217+angeruiizz@users.noreply.github.com>
2024-10-19 19:46:48 +0200

    parte bcn

--- bcnAnalysis.py
+++ bcnAnalysis.py
@@ -1,8 +1,9 @@
 import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt #per poder mostrar els grafics
+import seaborn as sns
 
-# Configurar pandas para mostrar todas las columnas
+#Configurar pandas para mostrar todas las columnas
 pd.set_option('display.max_columns', None)
 
 #Carregar les dades
@@ -20,7 +21,8 @@ columnas_a_eliminar = [
     'number_of_reviews_ltm', 'first_review', 'last_review', 'license',
     'calculated_host_listings_count', 'calculated_host_listings_count_entire_homes',
     'calculated_host_listings_count_private_rooms', 'calculated_host_listings_count_shared_rooms',
-    'reviews_per_month'
+    'reviews_per_month', 'name', 'description', 'picture_url'
+
 ]
 
 #Eliminar columnas que son irrelevantes
@@ -38,39 +40,99 @@ dataBcn['host_acceptance_rate'] = dataBcn['host_acceptance_rate'].str.replace('%
 dataBcn['host_acceptance_rate'] = dataBcn['host_acceptance_rate'].astype(float)
 
 #Pasar a boleano
-dataBcn['host_identity_verified'] = dataBcn['host_identity_verified'].replace({'t': True, 'f': False})
-dataBcn['instant_bookable'] = dataBcn['instant_bookable'].replace({'t': True, 'f': False})
+dataBcn['host_identity_verified'] = dataBcn['host_identity_verified'].str.strip().replace({'t': True, 'f': False})
+dataBcn['instant_bookable'] = dataBcn['instant_bookable'].str.strip().replace({'t': True, 'f': False})
 
-# Verifica el resultado
-print(dataBcn[['price','host_response_rate','host_acceptance_rate','host_identity_verified','instant_bookable']].info())
-
-
-
-
-
-#Columnas que se usarn para el analisis
-#print("Columnes per fer l'analisis:")
-#print(dataBcn.columns)
-
-#dataBcn.hist('availability_365')
-#plt.show()
+#Transformar columna amenities en una lista
+dataBcn['amenities'] = dataBcn['amenities'].str.replace('{', '', regex=False)
+dataBcn['amenities'] = dataBcn['amenities'].str.replace('}', '', regex=False)
+dataBcn['amenities'] = dataBcn['amenities'].str.replace('"', '', regex=False)
+dataBcn['amenities'] = dataBcn['amenities'].str.split(',').apply(np.array)
 
 
-#Histograma relaciones
-#
-#dataBcn.plot(kind='scatter', x='accommodates', y='availability_365', alpha=0.5)
-#plt.title("Relación entre el Precio y la Disponibilidad")
-#plt.xlabel("Accommodates")
-#plt.ylabel("Días disponibles al año")
-#plt.show()
+#Mostrar info de las columnas
+print(dataBcn[['price','host_response_rate','host_acceptance_rate','host_identity_verified','instant_bookable', 'amenities']].info())
+
+#Guardar los datos transformados
+dataBcn.to_csv("CityFiles/barcelona/tarnsformado.csv", sep = ';', index=True)
+
+#Mostrar dataset
+#print(dataBcn.head())
+
+# Arrays con las listas de variables
+variables_numericas = [
+    'host_response_rate', 'host_acceptance_rate',
+    'host_total_listings_count', 'accommodates',
+    'bathrooms', 'bedrooms', 'beds', 'price', 'minimum_nights', 'maximum_nights',
+    'minimum_nights_avg_ntm', 'maximum_nights_avg_ntm', 'availability_365',
+    'number_of_reviews', 'review_scores_rating', 'review_scores_accuracy',
+    'review_scores_cleanliness', 'review_scores_checkin', 'review_scores_communication',
+    'review_scores_location', 'review_scores_value'
+]
+
+variables_categoricas = [
+    'name', 'description', 'picture_url', 'neighbourhood',
+    'neighbourhood_cleansed', 'neighbourhood_group_cleansed', 'property_type',
+    'room_type', 'amenities'
+]
+
+variables_booleanas = [
+    'host_identity_verified', 'instant_bookable'
+]
+#Analisis variables numeriques
+#Estadisticas descriptivas
+data_numericas = dataBcn[variables_numericas]
+estadisticas_numericas = data_numericas.describe()
+print(estadisticas_numericas)
 
 
 
+#Distribución de las variables numéricas mas interesantes
+#Distribución de la variable price
+plt.figure(figsize=(10, 6))
+sns.histplot(dataBcn['price'], bins=30, kde=True)
+plt.title('Distribución de la variable price')
+plt.xlabel('price')
+plt.ylabel('Frecuencia')
+plt.show()
 
+#Distribución de la variable review_scores_rating
+plt.figure(figsize=(10, 6))
+sns.histplot(dataBcn['review_scores_rating'], bins=30, kde=True)
+plt.title('Distribución de la variable review_scores_rating')
+plt.xlabel('review_scores_rating')
+plt.ylabel('Frecuencia')
+plt.show()
 
-#Per passar-ho a csv
-#dataBcn.to_csv("CityFiles/barcelona/tarnsformado.csv", sep = ';', index=True)
+#Distribución de la variable availability_365
+plt.figure(figsize=(10, 6))
+sns.histplot(dataBcn['availability_365'], bins=30, kde=True)
+plt.title('Distribución de la variable availability_365')
+plt.xlabel('availability_365')
+plt.ylabel('Frecuencia')
+plt.show()
 
+#Distribución de la variable number_of_reviews
+plt.figure(figsize=(10, 6))
+sns.histplot(dataBcn['number_of_reviews'], bins=30, kde=True)
+plt.title('Distribución de la variable number_of_reviews')
+plt.xlabel('number_of_reviews')
+plt.ylabel('Frecuencia')
+plt.show()
 
+##QUITAR OUTLIERS DE LA VARIABLE PRICE
+# Suponiendo que ya tienes tu dataframe con la variable "price"
+# Filtrar eliminando outliers usando los percentiles
+lower_bound = data['price'].quantile(0.01)  # 1er percentil
+upper_bound = data['price'].quantile(0.99)  # 99º percentil
 
+# Crear un nuevo dataframe sin outliers
+data_filtered = data[(data['price'] >= lower_bound) & (data['price'] <= upper_bound)]
+
+# Representación gráfica sin outliers
+sns.histplot(data_filtered['price'], kde=True)
+plt.title('Distribución de la variable price sin outliers')
+plt.xlabel('price')
+plt.ylabel('Frecuencia')
+plt.show()
 
