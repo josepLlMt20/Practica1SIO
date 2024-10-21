5876d72ebb26cc8d724693bd84c759f559587d8c
Angelina Ruiz <125549217+angeruiizz@users.noreply.github.com>
2024-10-22 00:24:35 +0200

    Angelina: Safegeix possibilitat de per capes, pero no pinta per preu, hem de netejar preu

--- main.py
+++ main.py
@@ -10,19 +10,28 @@ dataBcn = pd.read_csv("CityFiles/barcelona/listings.csv")
 dataBer = pd.read_csv("CityFiles/berlin/listings.csv")
 dataBuAr = pd.read_csv("CityFiles/buenos aires/listings.csv")
 
-print(dataBcn.head())
-print(dataBcn.info())
+# Cargar los datos
+dataBcn = pd.read_csv("CityFiles/barcelona/listings.csv")
 
-dataBcn.hist('availability_365')
-plt.show()
+# Eliminar columnas irrelevantes
+columnas_a_eliminar = [
+    'listing_url', 'scrape_id', 'last_scraped', 'source', 'neighborhood_overview',
+    'host_url', 'host_name', 'host_since', 'host_location', 'host_about',
+    'host_response_time', 'host_is_superhost', 'host_thumbnail_url', 'host_picture_url',
+    'host_neighbourhood', 'host_listings_count', 'host_verifications', 'host_has_profile_pic',
+    'bathrooms_text', 'minimum_minimum_nights', 'maximum_minimum_nights', 'minimum_maximum_nights',
+    'maximum_maximum_nights', 'calendar_updated', 'has_availability', 'availability_30',
+    'availability_60', 'availability_90', 'calendar_last_scraped', 'number_of_reviews_l30d',
+    'number_of_reviews_ltm', 'first_review', 'last_review', 'license',
+    'calculated_host_listings_count', 'calculated_host_listings_count_entire_homes',
+    'calculated_host_listings_count_private_rooms', 'calculated_host_listings_count_shared_rooms',
+    'reviews_per_month', 'name', 'description', 'picture_url'
+]
+dataBcn.drop(columns=columnas_a_eliminar, axis=1, inplace=True)
+dataBer.drop(columns=columnas_a_eliminar, axis=1, inplace=True)
+dataBuAr.drop(columns=columnas_a_eliminar, axis=1, inplace=True)
 
-#
-#
-# #Histograma relaciones
-# dataBcn.plot(kind='scatter', x='accommodates', y='availability_365', alpha=0.5)
-# plt.title("Relación entre el Precio y la Disponibilidad")
-# plt.xlabel("Accommodates")
-# plt.ylabel("Días disponibles al año")
-# plt.show()
+dataBcn.to_csv("CityFiles/barcelona/transformado.csv", sep=';', index=True)
+dataBuAr.to_csv("CityFiles/buenos aires/transformado.csv", sep=';', index=True)
+dataBer.to_csv("CityFiles/berlin/transformado.csv", sep=';', index=True)
 
-dataBcn.to_csv("CityFiles/barcelona/tarnsformado.csv", sep = ';', index=True)
